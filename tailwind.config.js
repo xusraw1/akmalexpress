43405a2c7bd912d91a7993f6b8d5d40abb8038b9
bw007 <abdulaziz00791@gmail.com>
2024-03-30 07:35:40 +0500

    screen resolution config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: ["./AkmalExpress/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {},
+    screens: {
+      'sm': '640px', // Small screens
+      'md': '768px', // Medium screens
+      'lg': '1024px', // Large screens
+      'xl': '1280px', // Extra large screens
+      // Add more breakpoints if needed
+    },
   },
   plugins: [
         require('flowbite/plugin')
