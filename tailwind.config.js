96fd5ab5611cb6e71564330c31a6673635adfb7b
SENIOR <developer@SENIORs-Mac-mini.local>
2026-03-05 10:04:21 +0500

    Новый redesign фронта! + пару поправок

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./AkmalExpress/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
+  content: [
+    "./templates/**/*.html",
+    "./akmalexpress/templates/**/*.html",
+    "./node_modules/flowbite/**/*.js",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Manrope", "sans-serif"],
+        display: ["Space Grotesk", "sans-serif"],
+      },
+      colors: {
+        brand: {
+          DEFAULT: "#0f766e",
+          warm: "#ea580c",
+          ink: "#132333",
+        },
+      },
+    },
   },
-  plugins: [
-        require('flowbite/plugin')
-    ],
-}
+  plugins: [require("flowbite/plugin")],
+};
