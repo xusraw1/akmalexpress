38463c4b7890347e34e8d4b84b353ac90e1b8011
SENIOR <developer@SENIORs-Mac-mini.local>
2026-03-10 16:36:25 +0500

    feat: automatically create superuser during render deploy

--- build.sh
+++ build.sh
@@ -6,3 +6,9 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Automatically create a superuser if DJANGO_SUPERUSER_USERNAME is set in the environment
+if [[ -n "${DJANGO_SUPERUSER_USERNAME}" ]]; then
+  echo "Creating superuser..."
+  python manage.py createsuperuser --noinput || echo "Superuser already exists."
+fi
